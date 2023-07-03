1a794014f3dd4c9bca83cf681e623c523795a1a5
guilherme <guicarvalhooliveira2468@gmail.com>
2023-07-03 19:57:11 +0100

    add

--- Js/rss.js
+++ Js/rss.js
@@ -5,31 +5,31 @@ var url = 'https://rss.tecmundo.com.br/feed';
     
             var frase = "";
             for (i = 0; i < objeto_json.items.length; i++){
-                frase += '<div class="card mb-2 p-3 mb-3 bg-body rounded " style="width: 100%;">'
+                frase += '<div class="card mb-2 p-3 mb-3 bg-body rounded " style="max-width: 100%;">'
                 frase += '<div class="row">'
-                frase += '<div class="col-md-12">'
-                frase += '<h2> <a class="card-title" href="' + objeto_json.items[i].link + '">'+ objeto_json.items[i].title +'</a></h2>' 
+                frase += '<div class="col-md-12 ">'
                 frase += '<img src="'+ objeto_json.items[i].enclosure.link +'" class="img-fluid rounded-start" alt="imgNoticia">'
-                frase += '<div class="col-md-12 col-sm-8">'
+                frase += '<h2> <a class="card-title" href="' + objeto_json.items[i].link + '">'+ objeto_json.items[i].title +'</a></h2>' 
+                frase += '<div class="col-md-12">'
                 frase += '<div class="card-body">'
                 frase += '<p>' + objeto_json.items[i].pubDate + '</p>'
                 frase += '</div>'
                 frase += '</div>'
                 frase += '</div>'
                 frase += '</div>'
-                frase += '</div>'
+                 frase += '</div>'
             };
             $("#box").html(frase);
 
             var card = "";
             for (i = 3; i < 4; i++){
-                card += '<div class="card col more"  style="width: 18rem; margin-top: 10px;padding:0">'
+                card += '<div class="card col more"  style="width: 18rem; margin-top: 10px;">'
                 card += '<img src="'+ objeto_json.items[i].enclosure.link +'" class="card-img-top" alt="img">'
                 card += '<div class="card-body">'
                 card += '<h5 class="card-title">'+ objeto_json.items[i].title + '</h5>'
-                card += '<p>' + objeto_json.items[i].description + '</p>'
-                card += ' </div>'
-                card += ' </div>'
+                card += '<a href="' + objeto_json.items[i].link +'" class="btn btn-primary">Saiba +</a>'
+                card += '</div>'
+                card += '</div>'
 
             };
             
